639d254e2f13e92fa32992ed5497166abb1c232b
Nick Melms <nick@craftedagency.com>
2025-03-15 12:43:17 -0400

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-import clerk from "@clerk/astro";
 
-export default defineConfig({
-  integrations: [clerk()],
-  adapter: node({ mode: "standalone" }),
-  output: "static",
-});
+// https://astro.build/config
+export default defineConfig({});
